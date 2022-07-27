c1bbf411903b3d6561fb44248899b332cdc766fa
Sommer Shearer <sshearersommer@gmail.com>
2022-07-26 20:23:46 -0600

    linking

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,11 @@
+import type { LinksFunction } from "@remix-run/node"; 
+
+import styles from "../styles/Index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
