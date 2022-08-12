914a838198e15c0f9f7011b1affed29ce87e7fda
Sommer Shearer <sshearersommer@gmail.com>
2022-08-11 21:56:53 -0600

    get token

--- app/routes/instagram.tsx
+++ app/routes/instagram.tsx
@@ -11,19 +11,46 @@ import Sidebar from "~/components/sidebar";
 
 import InstagramIcon from '@mui/icons-material/Instagram';
 
-const appId = 448646527197070;
+const appId = '448646527197070';
+const appS = '0e8ba0033b150898c74fd8996d6a17a1';
 const redirectUri = 'https://the-awesome-sommerariel-site.netlify.app/instagram';
 
 export default function instagramRoute(): JSX.Element  {
-
-    useEffect (() => {
-
-
-    }, []);
-
+    const [isReady, setIsReady] = useState(false);
+    const [code, setCode] = useState<string | null>(null);
     const [searchParams] = useSearchParams();
 
-    console.log({searchParams})
+    console.log("instacode", searchParams.get('code'))
+
+    const getToken = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ 
+                client_id: appId, 
+                client_secret: appS, 
+                grant_type: 'authorization_code',
+                redirect_uri: {redirectUri}, 
+                code: {code}
+            })
+        };
+        fetch('https://api.instagram.com/oauth/access_token', requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+          console.log('Success:', result);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
+
+    useEffect (() => {
+        setCode(searchParams.get('code'));
+        if (code) {
+            getToken();
+        }
+    }, [searchParams, code, setCode, getToken]);
+
 
     const onSearch = () => {
         const url = `https://api.instagram.com/oauth/authorize?client_id=${appId}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`;
