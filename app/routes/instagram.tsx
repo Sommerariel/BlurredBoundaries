468b8ce2ff1a489dd12a3e50bc0341c5dd172d87
Sommer Shearer <sshearersommer@gmail.com>
2022-08-11 20:19:58 -0600

    get query param

--- app/routes/instagram.tsx
+++ app/routes/instagram.tsx
@@ -1,7 +1,41 @@
+import { 
+    useState, 
+    useEffect, 
+  } from 'react';
+
+import { useSearchParams } from "@remix-run/react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+import Sidebar from "~/components/sidebar";
+
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+const appId = 448646527197070;
+const redirectUri = 'https://the-awesome-sommerariel-site.netlify.app/instagram';
+
 export default function instagramRoute(): JSX.Element  {
+
+    useEffect (() => {
+
+
+    }, []);
+
+    const [searchParams] = useSearchParams();
+
+    console.log({searchParams})
+
+    const onSearch = () => {
+        const url = `https://api.instagram.com/oauth/authorize?client_id=${appId}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`;
+        window.open(url, '_blank', 'noopener,noreferrer');
+    }
+
     return (
         <>
-            Instagram
+            <Box sx={{ display: 'flex' }}>
+                <Sidebar />
+                <Button variant="contained" startIcon={<InstagramIcon />} onClick={onSearch}>Get your Public Info</Button>
+            </Box>
         </>
     )
 };
